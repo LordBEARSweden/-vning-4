c7162a95993b91d059a6c7e583f5ffccccefb8c7
Björn Andersson <lord_bear@hotmail.com>
2021-02-08 13:21:59 +0100

    kaos

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 namespace SkalProj_Datastrukturer_Minne
 {
     class Program
     {
+
         /// <summary>
         /// The main method, vill handle the menues for the program
         /// </summary>
@@ -18,7 +20,9 @@ namespace SkalProj_Datastrukturer_Minne
                     + "\n2. Examine a Queue"
                     + "\n3. Examine a Stack"
                     + "\n4. CheckParanthesis"
-                    + "\n0. Exit the application");
+                    + "\n5. Reverse text"
+                    + "\n0. Exit the application"
+                    );
                 char input = ' '; //Creates the character input to be used with the switch-case below.
                 try
                 {
@@ -42,6 +46,9 @@ namespace SkalProj_Datastrukturer_Minne
                         break;
                     case '4':
                         CheckParanthesis();
+                        break; 
+                    case '5':
+                        ReverserText();
                         break;
                     /*
                      * Extend the menu to include the recursive 
@@ -57,11 +64,56 @@ namespace SkalProj_Datastrukturer_Minne
             }
         }
 
+        private static void ReverserText()
+        {
+            var stack = new Stack<Char>();
+            var input = Console.ReadLine();
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+           
+        }
+
         /// <summary>
         /// Examines the datastructure List
         /// </summary>
         static void ExamineList()
         {
+            List<string> theList = new List<string>();
+            bool menu = true;
+            do
+            {
+            Console.WriteLine("press + to add to the list or press - to remove from the list Q for exit");
+            string input = Console.ReadLine();
+            char nav = input[0];
+            string value = input.Substring(1);
+
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        Console.WriteLine("Added: " + value);
+                        Console.WriteLine($"Count: {theList.Count}");
+                        Console.WriteLine(theList.Capacity); 
+                        break;
+
+                    case '-':
+                        theList.Remove(value);
+                        Console.WriteLine("Subrtacted: " + value);
+                        Console.WriteLine($"Count: {theList.Count}");
+                        Console.WriteLine(theList.Capacity);
+                        break;
+                    
+                    case 'Q':
+                        menu = false;
+                        break;
+                }
+
+            } while (menu);
+           
             /*
              * Loop this method untill the user inputs something to exit to main menue.
              * Create a switch statement with cases '+' and '-'
@@ -85,6 +137,35 @@ namespace SkalProj_Datastrukturer_Minne
         /// </summary>
         static void ExamineQueue()
         {
+                Queue<string> theQueue = new Queue<string>();
+            bool menu = true;
+            do
+            {
+                Console.WriteLine("Examine the queue + to add - to remove");
+                var input = Console.ReadLine();
+                var value = input.Substring(1);
+
+
+                switch (input[0])
+                {
+                    case '+':
+                        theQueue.Enqueue(value);
+                        Console.WriteLine("In Queue: " + value);
+                        Console.WriteLine($"Count: {theQueue.Count}");
+                        break;
+
+                    case '-':
+                        if (theQueue.Count != 0)
+                        theQueue.Dequeue();
+                        Console.WriteLine($"Count: {theQueue.Count}");
+                        break;
+
+                    case 'Q':
+                        menu = false;
+                        break;
+                }
+
+            } while (menu);
             /*
              * Loop this method untill the user inputs something to exit to main menue.
              * Create a switch with cases to enqueue items or dequeue items
@@ -97,6 +178,37 @@ namespace SkalProj_Datastrukturer_Minne
         /// </summary>
         static void ExamineStack()
         {
+            Stack<string> theStack = new Stack<string>();
+            bool menu = true;
+            
+            
+            do
+            {
+               Console.WriteLine("Examine the queue + to push - to pop");
+               string input = Console.ReadLine();
+          
+               
+
+                switch (input[0])
+                {
+                    case '+':
+                        theStack.Push(input);
+                        Console.WriteLine("Push: " + input);
+                      
+                        break;
+
+                    case '-':
+                        theStack.Pop();
+                        Console.WriteLine("pop: " + input);
+                   
+                        break;
+
+                    case 'Q':
+                        menu = false;
+                        break;
+                }
+
+            } while (menu);
             /*
              * Loop this method until the user inputs something to exit to main menue.
              * Create a switch with cases to push or pop items
@@ -111,9 +223,60 @@ namespace SkalProj_Datastrukturer_Minne
              * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
+            var input = "(())";
+            var stack = new Stack<char>();
+            var dict = new Dictionary<char, char>
+            {
+                {'(', ')' }
+            };
+
+            foreach (var c in input)
+            {
+
+                if (dict.ContainsKey(c))
+                    stack.Push(c);
+
+
+            }
+
+            //Använd stack
+
+            //loopa på inout
+
+            //om öppnare spara undan annars fel
+            //Stängare sparas aldrig
+            //Har vi något kvar ? fel
+
+
 
         }
-
     }
 }
+
+
+//Frågor
+//1: Stacken är en trave staplat på varandra som är självunderhållande. Den har koll på vilka metoder och
+//anrop som körs och rensar bort dem allt efersom den blir klar med dem. Stacken börjar från början och 
+//arbetar sig sedan igenom metoderna och anropen i ordning. Stacken är självunderhållande.
+//Heapen har tillgång till allt vilket är en skillnad från stacken. Den kan snabbt komma åt det vi vill ha.
+//Den behöver även oroa sig för garbage collection.
+
+//2: Typer som kommer ifrån System.valuetype några exempel är Int, bool, Char, Float, Enum och Double.
+//   Reference types ärver från System.object. Referenstyper är tex. Class, Interface, Object, Delegate och string.
+//   Reference typer lagrar alltid på Heapen medans Value types kan lagras i både Stacken och på Heapen.
+
+//3: Den första returnerar 3 eftersom x är satt som tre.
+//   Den andra returnerar 4 eftersom y är lika med x och y är satt till 4 senare i koden.
+
+//Frågor Examine list
+// 2:Den ökar på rad 81-82. alltså efter att något blivit registrerat.
+// 3: Den ökar med 4
+// 4: vet ej
+// 5: Nej den minskar inte
+// 6: När man jobbar med fasta listor?
+
+//Fråga Examine stack
+// 1: Eftersom det skulle innebära att den som kom till kön först inte skulle få gå förrän alla andra gått.
+
+
 
