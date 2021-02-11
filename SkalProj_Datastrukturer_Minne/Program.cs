7930e772c01654dcf174eec16e0b2c4dde50b63c
Björn Andersson <lord_bear@hotmail.com>
2021-02-11 15:39:25 +0100

    nytt försök

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -8,6 +8,7 @@ namespace SkalProj_Datastrukturer_Minne
 
         /// <summary>
         /// The main method, vill handle the menues for the program
+        /// hejsan
         /// </summary>
         /// <param name="args"></param>
         static void Main()
